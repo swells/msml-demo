7cb86b2d6ee23a27d1036ef4e5e47842b409a1b1
Sean Wells <sean.wells.sc@gmail.com>
2015-05-27 07:51:03 -0700

    first commit

---
+++ examples/ui.js
@@ -0,0 +1,31 @@
+var ui = {
+    /* Bind functions and values */
+    ready: function() {
+
+    },
+
+    onEachFeatureHandler: function(feature, layer) {
+        layer.bindPopup(feature.properties['Name.x'] + '<br>' + feature.properties.City + '<br> Cluster: ' + feature.properties.cluster);
+    },
+
+    pointToLayerHandler: function(feature, latlng) {
+        var CLUSTER_COLORS = {
+            1: '#F8766D',
+            2: '#C49A00',
+            3: '#53B400',
+            4: '#02C094',
+            5: '#00B6EB',
+            6: '#A58AFF',
+            7: '#FB61D7',
+        };
+
+        return L.circleMarker(latlng, {
+            radius: 8,
+            fillColor: CLUSTER_COLORS[feature.properties.cluster],
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.7
+        });
+    }
+};
--- ui/ui.js
+++ ui/ui.js
@@ -1,31 +1,8 @@
 var ui = {
-    /* Bind functions and values */
-    ready: function() {
+    /* Bind additional functions and values to the UI */
 
-    },
-
-    onEachFeatureHandler: function(feature, layer) {
-        layer.bindPopup(feature.properties['Name.x'] + '<br>' + feature.properties.City + '<br> Cluster: ' + feature.properties.cluster);
-    },
-
-    pointToLayerHandler: function(feature, latlng) {
-        var CLUSTER_COLORS = {
-            1: '#F8766D',
-            2: '#C49A00',
-            3: '#53B400',
-            4: '#02C094',
-            5: '#00B6EB',
-            6: '#A58AFF',
-            7: '#FB61D7',
-        };
-
-        return L.circleMarker(latlng, {
-            radius: 8,
-            fillColor: CLUSTER_COLORS[feature.properties.cluster],
-            color: "#000",
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.7
-        });
-    }
+    /*
+     * Element lifecycle method
+     */
+    ready: function() {}
 };
